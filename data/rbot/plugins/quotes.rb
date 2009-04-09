f7b07fe0eee7c6cbbe8ee8246813f8e9183b6277
David Gadling <dave@toasterwaffles.com>
2009-04-09 09:58:26 -0700

    quotes: Don't need to CGI.escape a timestamp.

--- data/rbot/plugins/quotes.rb
+++ data/rbot/plugins/quotes.rb
@@ -144,7 +144,7 @@ class QuotePlugin < Plugin
         }
         
         quotes.compact.each{ |q|
-          bodySubs = { "%%timestamp%%" => CGI.escapeHTML(q.date), 
+          bodySubs = { "%%timestamp%%" => q.date, 
                               "%%id%%" => CGI.escapeHTML(q.num.to_s),
                           "%%author%%" => CGI.escapeHTML(q.source[0, q.source.index("!")]),
                          "%%channel%%" => CGI.escapeHTML(channel),
