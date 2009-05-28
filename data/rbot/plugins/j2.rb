894c3701e6df0e3b6dc0cc24f6bd119590bdf9f4
archer007 <dslguy14@yahoo.com>
2009-05-28 02:51:49 -1000

    Added more !newtopic functionality, still incomplete.

--- data/rbot/plugins/j2.rb
+++ data/rbot/plugins/j2.rb
@@ -21,25 +21,39 @@ class J2Plugin < Plugin
   def help(plugin, topic="")
     [
       _("j2 => Prints this help menu"),
-      _("j2 newposts => gets new posts from forums.joe.to"),
+      _("j2 newtopics => gets new posts/topics from forums.joe.to"),
     ].join(", ")
   end
 
+	def baconator(m, params)
+		m.act('stuffs ' + m.sourcenick + '\'s mouth full of bacon')
+	end
+  
   # ugh. this needs to be completely re-done so that I can refer to sibling elements and 
-  # get author names as well as the unread links. Can I specify a rango of elements to extract from??
-	def j2(m, params)
+  # get author names as well as the unread links. Can I specify a range of elements to extract from??
+	def newtopics(m, params)
 	 doc = Hpricot(open("http://forums.joe.to/search.php?search_id=newposts"))
 	 topics = doc.search("//a[@class='topictitle']")
 	
+	newtopics = Array.new(30)
+	
+	iterator = 0
 	topics.each do |topic|
-		m.reply topic.inner_html
+		#m.reply topic.inner_html
+		newtopics[iterator] = topic.inner_html
+		iterator = iterator + 1
+	end
+	
+	for i in 0..params[:number_of_topics] - 1
+		m.reply newtopics[i]
 	end
  end
 end
 
 plugin = J2Plugin.new
 
-plugin.map "j2"
+plugin.map 'j2 newtopics [number_of_topics]', :action=>'newtopics', :private => false, :defaults => { :number_of_topics => 5 }
+plugin.map 'baconator'
 	#:action => 'newtopics'
 #plugin.map "j2 nt",
 #  :action => 'newtopics'
